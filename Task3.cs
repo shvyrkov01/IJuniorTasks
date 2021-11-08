5b006dea0e71a49dc674776e3d59812eb28e7066
Alexander <48875372+shvyrkov01@users.noreply.github.com>
2021-11-08 15:47:26 +0300

    Add files via upload

---
+++ Task3.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace task3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int picturesInRowAmount = 3;
+            int picturesAmount = 52;
+
+            int rowsAmount = (int)(picturesAmount / picturesInRowAmount);
+            int picturesOutOfRangeAmount = picturesAmount - (rowsAmount * picturesInRowAmount);
+
+            Console.WriteLine($"Кол-во полностью заполненных рядов: {rowsAmount}");
+            Console.WriteLine($"Кол-во картинок сверх меры: {picturesOutOfRangeAmount}");
+        }
+    }
+}

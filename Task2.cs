547f9df59a56f5da007178fa9ca43782569ccd57
Alexander <48875372+shvyrkov01@users.noreply.github.com>
2021-11-08 14:08:44 +0300

    Add files via upload

---
+++ Task2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int serviceTimeMinutes = 10;
+
+            Console.WriteLine("Введите кол-во человек в очереди перед вами: ");
+
+            if (!int.TryParse(Console.ReadLine(), out int numberPersons))
+                throw new ArgumentException("Вы должны ввести число!");
+
+            TimeSpan waitingTime = new TimeSpan(0, serviceTimeMinutes * numberPersons, 0);
+
+            Console.WriteLine($"Время вашего ожидания состовляет: {waitingTime.Hours} часа {waitingTime.Minutes} минут");
+        }
+    }
+}

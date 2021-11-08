66d062cf6e41f136259de5d19b779e88da7b19c0
Alexander <48875372+shvyrkov01@users.noreply.github.com>
2021-11-08 14:05:01 +0300

    Add files via upload

---
+++ Task4.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace task4
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int goldAmount;
+            int diamondAmount;
+
+            int diamondCost = 3;
+
+            Console.WriteLine("Введите начальное кол-во золота: ");
+
+            goldAmount = TryConvertToInt(Console.ReadLine());
+
+            Console.WriteLine($"Цена одного Кристалла: {diamondCost}\nВведите количество Кристаллов, которое хотите приобрести:");
+
+            int diamondTarget = TryConvertToInt(Console.ReadLine());
+
+            goldAmount -= (diamondTarget * diamondCost);
+            diamondAmount = diamondTarget;
+
+            Console.WriteLine($"Кошелек\nЗолота:{goldAmount}\nКристаллов: {diamondAmount}");
+        }
+
+
+        private static int TryConvertToInt(string inputText)
+        {
+            if (!int.TryParse(inputText, out int value))
+                throw new ArgumentException("Вы должны ввести число!");
+
+            return value;
+        }
+    }
+}
